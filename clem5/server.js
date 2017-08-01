8d3b13a476c729a201f6bc246dfbde7980dea04a
Kevin Smith <ksjazzguitar@yahoo.com>
2017-07-31 22:42:02 -0700

    more clem work

---
+++ clem5/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express'),
+	routes = require('./app/routes/index.js'),
+	mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/clementinejs');
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+routes(app);
+
+var port = 8080;
+app.listen(port, function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
